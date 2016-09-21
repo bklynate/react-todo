9093aa08bb8a9b7c9313014fbae3e48e221fd477
bklynate <nathanielcfa@gmail.com>
2016-09-21 19:21:15 -0400

    Edit TodoList

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,10 +5,27 @@ var React = require('react'),
     ReactDOM = require('react-dom');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
   // body...
   it('should exist', () => {
     expect(TodoList).toExist();
   });
+
+  it('should render a Todo component for each todo passed to it', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'developer test text'
+      },
+      {
+        id: 2,
+        text: "Smell what the rock is cooking"
+      }
+    ];
+    var todolist = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todoComponents = TestUtils.scryRenderedComponentsWithType(todolist, Todo);
+    expect(todoComponents.length).toBe(todos.length);
+  })
 });
